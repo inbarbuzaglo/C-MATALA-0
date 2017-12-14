a99a1149f4b3b56468aa5aee6c0492b7a5f0ca60
inbarbuzaglo <33480360+inbarbuzaglo@users.noreply.github.com>
2017-12-14 15:32:24 +0200

    Update ListFromCSV.java

--- ListFromCSV.java
+++ ListFromCSV.java
@@ -9,30 +9,47 @@ package matala0;
 	import java.util.Comparator;
 	import java.util.Scanner;
 
-	import matala0.Position;
-	import matala0.StartLine;
-	import matala0.WifiData;
-	import matala0.line;
-
+import Headline_Obj.Position;
+import Headline_Obj.StartLine;
+import Headline_Obj.WifiData;
+import Headline_Obj.line;
+/**
+ * 
+ * @author Inbar
+ *This class scan each file in the folder,and build an ArrayList that unites all the files.
+ *The file includes the best ten Wifi singnals that were found.
+ */
 	public class ListFromCSV {
 		
-		static ArrayList<line> mergedfiles;
+		public static ArrayList<line> mergedfiles;
+		public static ArrayList<line_OUTPUT_FORMAT> outputList = new ArrayList<line_OUTPUT_FORMAT>();
 		static ArrayList<line> currentWork;
-
-		public ListFromCSV(File folder) throws IOException { // TODO: לטפל במה שהוא זורק
+		private static String outputFilePath = "C:/test1/output/out.csv";
+		
+		public ListFromCSV() {
+			this.mergedfiles=null;
+			this.currentWork=null;
+		}
+		
+		public ListFromCSV(File folder) throws IOException { 
 			mergedfiles = new ArrayList<line>();
 			currentWork = new ArrayList<line>();
 			listFilesForFolder(folder);
 			SortByPosition();
 			currentWork.add(mergedfiles.get(0));
-			CSV csv = new CSV(currentWork);
-			for(int i=0;i<mergedfiles.size();i++) {
-				while(i<mergedfiles.size()-1 && mergedfiles.get(i).getPos().equals(mergedfiles.get(i+1).getPos())) {
+			CSV csv = new CSV(outputFilePath,currentWork);
+			CSV.writeCSVfirstLine();
+			for(int i=0;i<mergedfiles.size();i++) 
+			{
+				while(i<mergedfiles.size()-1 && 
+						mergedfiles.get(i).getPos().equals(mergedfiles.get(i+1).getPos()))
+				{
 					currentWork.add(mergedfiles.get(i+1));
 					i++;
 				}
-				sortByWifiRSSI();
+				currentWork = sortByWifiRSSI(currentWork);
 				if(currentWork.isEmpty())currentWork.add(mergedfiles.get(i));
+				createOutputFormat(currentWork);
 				csv.updateList(currentWork);
 				csv.write();
 				currentWork.clear();
@@ -40,12 +57,30 @@ package matala0;
 			csv.close();
 		}
 
-		public void sortByWifiRSSI() {
-			Collections.sort(currentWork, new Comparator<line>() {
+		public ArrayList<line> getMergedfiles() {
+			return mergedfiles;
+		}
+
+		public  void setMergedfiles(ArrayList<line> mergedfiles) {
+			ListFromCSV.mergedfiles = mergedfiles;
+		}
+
+		public static ArrayList<line> sortByWifiRSSI(ArrayList<line> list) {
+			Collections.sort(list, new Comparator<line>() {
 				public int compare(line o1, line o2) {
 					return String.valueOf(o1.getWifi().getRSSI()).compareTo(String.valueOf(o2.getWifi().getRSSI()));
 				}
 			});
+			return list;
+		}
+		
+		public static ArrayList<line> sortByWifiMAC(ArrayList<line> list) {
+			Collections.sort(list, new Comparator<line>() {
+				public int compare(line o1, line o2) {
+					return String.valueOf(o1.getWifi().getMAC()).compareTo(String.valueOf(o2.getWifi().getMAC()));
+				}
+			});
+			return list;
 		}
 
 		// listFilesForFolder check if the files format is correct (in folder).
@@ -86,7 +121,7 @@ package matala0;
 
 		}
 		
-		
+		//Collection.sort uses the methods of Position to sort by position
 		public void SortByPosition() {
 			Collections.sort(currentWork, new Comparator<line>() {
 				public int compare(line o1, line o2) {
@@ -102,5 +137,13 @@ package matala0;
 			return (dotIndex == -1) ? "" : fileName.substring(dotIndex + 1);
 		}
 
+		
+		public void createOutputFormat(ArrayList<line> list){
+			outputList.add(new line_OUTPUT_FORMAT(list.get(0).getStartLine(), list.get(0).getPos(), new ArrayList<WifiData>()));
+			for (int i = 0; i < list.size() && i < 10; i++) {
+				outputList.get(outputList.size()-1).addWifi(list.get(i).getWifi());
+			}
+		}
+		
 	}
-
+	
