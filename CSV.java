0f249e5c61f15798c8765fb2cf0928f5a920a9bd
inbarbuzaglo <33480360+inbarbuzaglo@users.noreply.github.com>
2017-12-14 15:31:19 +0200

    Update CSV.java

--- CSV.java
+++ CSV.java
@@ -1,26 +1,49 @@
 package matala0;
 
-	import java.io.FileWriter;
+import java.io.FileWriter;
 	import java.io.IOException;
 	import java.util.ArrayList;
 
-	import matala0.line;
-
+import Headline_Obj.Position;
+import Headline_Obj.WifiData;
+import Headline_Obj.line;
+/**
+ * 
+ * @author Inbar
+ *This class build the csv file according to the requested format.
+  the first row contains headlines And below the information itself
+ */
 	public class CSV {
 
 		static int MAX_WIFI_NETWORKES = 10;
 		static FileWriter writer;
 		private ArrayList<line> list;
-		private static String outputFilePath = "C://test/test.csv";
+		private static String outputFilePath;
 
-		public CSV(ArrayList<line> list) {
+		public static String getOutputFilePath() {
+			return outputFilePath;
+		}
+
+		public CSV(String outputFilePath) {
 			try {
+				CSV.outputFilePath = outputFilePath;
 				writer = new FileWriter(outputFilePath, true); // true - the writer is appendable
-			} catch (IOException e) {
+			} 
+			catch (IOException e) {
 				System.out.println("cannot write to file at: " + outputFilePath);
 				System.out.println("errorCode: " + e.getMessage());	
 			}
-			writeCSVfirstLine();
+		}
+		
+		public CSV(String outputFilePath,ArrayList<line> list) {
+			try {
+				CSV.outputFilePath = outputFilePath;
+				writer = new FileWriter(outputFilePath, true); // true - the writer is appendable
+			} 
+			catch (IOException e) {
+				System.out.println("cannot write to file at: " + outputFilePath);
+				System.out.println("errorCode: " + e.getMessage());	
+			}
 			this.list = list;
 		}
 		
@@ -42,7 +65,7 @@ package matala0;
 		}
 
 		// writeCSVfirstLine write the titles
-		private static void writeCSVfirstLine() {
+		public static void writeCSVfirstLine() {
 			try {
 				writer.append("Time,ID,Lat,lon,Alt,#WiFi networks,");
 				for (int i = 1; i <= 10; i++) {
@@ -68,10 +91,36 @@ package matala0;
 		}
 
 		// writeWifiNet write the information about the WIFI points scanned (up to 10)
-		private static void writeWifiNet(line line) {
-			try {
+		public void writeWifiNet(line line) {
+			try
+			{
 				writer.append(line.getWifi().toCsv());
-			} catch (IOException e) {
+			} 
+			catch (IOException e) {
+				System.out.println("cannot write to file at: " + outputFilePath);
+				System.out.println("errorCode: " + e.getMessage());	
+			}
+		}
+		
+		public void writeWifiNetArray(line_OUTPUT_FORMAT line) {
+			try
+			{
+				for (WifiData wifi : line.getWifiArray()) {
+					writer.append(wifi.toCsv());
+				}
+			} 
+			catch (IOException e) {
+				System.out.println("cannot write to file at: " + outputFilePath);
+				System.out.println("errorCode: " + e.getMessage());	
+			}
+		}
+		
+		public void writePos(Position pos) {
+			try
+			{
+				writer.append(pos.toString());
+			} 
+			catch (IOException e) {
 				System.out.println("cannot write to file at: " + outputFilePath);
 				System.out.println("errorCode: " + e.getMessage());	
 			}
@@ -90,4 +139,14 @@ package matala0;
 			
 		}
 
+		public void writeNL() {
+			try {
+				writer.append("\r\n");
+			} catch (IOException e) {
+				System.out.println("cannot write to file at: " + outputFilePath);
+				System.out.println("errorCode: " + e.getMessage());	
+			}
+		}
+
 	}
+
