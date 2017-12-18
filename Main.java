0078e33107c14030b09eda9c30aa808292ea46b8
DESKTOP-4I9U2N8\Inbar <inbarbuzaglo@gmai.com>
2017-12-18 21:19:38 +0200

    # WARNING: head commit changed in the meantime h

--- Main.java
+++ Main.java
@@ -2,22 +2,31 @@ package CSV;
 
 
 
-	import java.io.File;
-	import java.io.IOException;
+import java.io.File;
+import java.io.IOException;
 
+import Algo.Algo;
+import Algo.Algo2;
+/**
+ * 
+ * @author Inbar
+ *In this class we open the folder where the Wigle application files are located
+ */
 	public class Main {
 
-		final static File folder = new File("C:");
+		final static File folder = new File("C:/test1/input");
 		
 		public static void main(String[] args) {
 			try {
-				new ListFromCSV(folder);// makes arraylist from input csv file and uses CSV class to arrange by parameterms
+				// makes arraylist from input csv file and uses CSV class to arrange by parameterms
+				new ListFromCSV(folder);
 				
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-					Algo alg = new Algo(ListFromCSV.mergedfiles);
+			
+			Algo alg = new Algo(ListFromCSV.mergedfiles);
 			Algo2 alg2 = new Algo2(ListFromCSV.outputList);
 		}
 
