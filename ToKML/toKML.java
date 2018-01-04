db439a51f4e7ac5c43f5c0677d612078d62b7f4e
Meytal Levin <33618164+Meyt3893@users.noreply.github.com>
2018-01-04 20:54:19 +0200

    Update toKML.java

--- ToKML/toKML.java
+++ ToKML/toKML.java
@@ -1,52 +1,66 @@
-package ToKML;
+package GUI;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
+import Algo.Algo;
+import Algo.Algo2;
+import CSV.ListFromCSV;
+import Filter.Filter;
+import KML.toKML;
 
-	import java.io.File;
-	import java.io.FileWriter;
-	import java.io.IOException;
-
-	import de.micromata.opengis.kml.v_2_2_0.Document;
-	import de.micromata.opengis.kml.v_2_2_0.Kml;
-
-	public class toKML {
-		static FileWriter writer;
-		static String FILENAME = "C://test/test.csv/";
-		static File f;
-		static Kml kml;
-		static Document document;
-
-		public static void main(String[] args) throws IOException {
-			openKML();
-			Filter.choosefilter();
-			closeKML();
-
-		}
-
-		// create a kml document to write to
-		public static void openKML() {
-			kml = new Kml();
-			document = kml.createAndSetDocument();
-		}
-
-		// AddPlacemark write the information about each point at KML file
-		public static void AddPlacemark(String name, String lat, String lon, String alt) {
-			document.createAndAddPlacemark().withName(name).createAndSetPoint().addToCoordinates(Double.valueOf(lat),
-					Double.valueOf(lon), Double.valueOf(alt));
-		}
-
-		// write kml document to a file
-		public static void closeKML() {
-			try {
-				kml.setFeature(document);
-				kml.marshal(new File("C:/Users/dimos/Desktop/hw/console/output/outputkml.kml"));
-				System.out.println("KML file has been generated, check output folder.");
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-
+public class wrapper {
+	
+	public wrapper()
+	{
+		
 	}
 
+	public void listfromfolder(File folder)
+	{
+		try {
+			// makes arraylist from input csv file and uses CSV class to arrange by parameterms
+			new ListFromCSV(folder);
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+	}
+	
+	public void tokml(File csv)
+	{
+	toKML.openKML();
+	try {
+		Filter.initScanners();
+		Filter.NonFilter();
+		toKML.closeKML();
+	} catch (FileNotFoundException e) {
+		System.out.println("cannot find csv input file, " + e.getMessage());
+	}
+	
+	}
 
+	
+	public void Algo1()
+	{
+		Algo alg = new Algo(ListFromCSV.mergedfiles);		
+	}
+	
+	public void Algo2()
+	{
+		Algo2 alg2 = new Algo2(ListFromCSV.outputList);	
+	}
+	
+	public void cleardata()
+	{
+		ListFromCSV.mergedfiles.clear();
+	}
+	
+	
+
+}
