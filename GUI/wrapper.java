8a52aeb82f4faa22b42cd55cc0d1f11158813fac
InbarBuzaglo <33480360+inbarbuzaglo@users.noreply.github.com>
2018-01-04 12:58:24 +0200

    Add files via upload

---
+++ GUI/wrapper.java
@@ -0,0 +1,67 @@
+package GUI;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import Algo.Algo;
+import Algo.Algo2;
+import CSV.ListFromCSV;
+import Filter.Filter;
+import KML.toKML;
+
+public class wrapper {
+	
+	public wrapper()
+	{
+		
+	}
+
+	public void listfromfolder(File folder)
+	{
+		try {
+			// makes arraylist from input csv file and uses CSV class to arrange by parameterms
+			new ListFromCSV(folder);
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+	}
+	
+	public void tokml(File csv)
+	{
+	toKML.openKML();
+	try {
+		Filter.initScanners();
+	} catch (FileNotFoundException e) {
+		System.out.println("cannot find csv input file, " + e.getMessage());
+	}
+	
+	Filter.NonFilter();
+	toKML.closeKML();
+
+	}
+
+	
+	public void Algo1()
+	{
+		Algo alg = new Algo(ListFromCSV.mergedfiles);		
+	}
+	
+	public void Algo2()
+	{
+		Algo2 alg2 = new Algo2(ListFromCSV.outputList);	
+	}
+	
+	public void cleardata()
+	{
+		ListFromCSV.mergedfiles.clear();
+	}
+	
+	
+
+}
