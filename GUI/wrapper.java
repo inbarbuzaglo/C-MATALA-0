9a86c0f69c1b378e4aeee79d7f4203fd3fb0202c
Meytal Levin <33618164+Meyt3893@users.noreply.github.com>
2018-01-07 21:46:44 +0200

    Update wrapper.java

--- GUI/wrapper.java
+++ GUI/wrapper.java
@@ -58,6 +58,49 @@ public class wrapper {
 			System.out.println("cannot find csv input file, " + e.getMessage());
 		}
 	}
+	
+	public void tokmllon (File csv,String lonfrom,String lonto)
+	{
+		toKML.openKML();
+		try {
+			PosFilter.initScanners();
+			Position posfrom=new Position("00",lonfrom,"00");
+			Position posto=new Position("00",lonto,"00");
+			PosFilter.lonFilter(posfrom.getLon(),posto.getLon());
+			toKML.closelonKML();
+		} catch (FileNotFoundException e) {
+			System.out.println("cannot find csv input file, " + e.getMessage());
+		}
+	}
+	
+	public void tokmlalt (File csv,String altfrom,String altto)
+	{
+		toKML.openKML();
+		try {
+			PosFilter.initScanners();
+			Position posfrom=new Position("00","00",altfrom);
+			Position posto=new Position("00","00",altto);
+			PosFilter.altFilter(posfrom.getAlt(),posto.getAlt());
+			toKML.closealtKML();
+		} catch (FileNotFoundException e) {
+			System.out.println("cannot find csv input file, " + e.getMessage());
+		}
+	}
+	
+	public void tokmltime(File csv,String timefrom,String timeto)
+	{
+		toKML.openKML();
+		try {
+			Filter.initScanners();
+			long lstart=Filter.StringDateToLong(timefrom);
+			long lend=Filter.StringDateToLong(timeto);
+			Filter.dateFilter(lstart, lend);
+			toKML.closetimeKML();
+		} catch (FileNotFoundException e) {
+			System.out.println("cannot find csv input file, " + e.getMessage());
+		}
+	}
+
 
 	
 	public void Algo1()
