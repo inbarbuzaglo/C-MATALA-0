06a34125149017bde75c80cf936f4c66d14c5d22
Meytal Levin <33618164+Meyt3893@users.noreply.github.com>
2018-01-07 21:45:02 +0200

    Update PosFilter.java

--- Filter/PosFilter.java
+++ Filter/PosFilter.java
@@ -52,6 +52,43 @@ public class PosFilter {
 		}
 		fileScanner.close();
 	}
+	
+	public static void lonFilter(double lon1, double lon2)
+	{
+		while (fileScanner.hasNext()) {
+			lineFromFile = fileScanner.nextLine();
+			spliter = lineFromFile.split(",");
+			Position pos2 = new Position(spliter[2], spliter[3], spliter[4]);
+			double lon=pos2.getLon();
+			System.out.println(lon);
+			System.out.println(lon1);
+			System.out.println(lon2);
+			if ((lon1<=lon)&&(lon<lon2)) 
+			{
+				printWifiNetworks();
+			}
+		}
+		fileScanner.close();
+	}
+	
+	public static void altFilter(double alt1, double alt2)
+	{
+		while (fileScanner.hasNext()) {
+			lineFromFile = fileScanner.nextLine();
+			spliter = lineFromFile.split(",");
+			Position pos2 = new Position(spliter[2], spliter[3], spliter[4]);
+			double alt=pos2.getAlt();
+			
+			if ((alt1<=alt)&&(alt<alt2)) 
+			{
+				printWifiNetworks();
+			}
+		}
+		fileScanner.close();
+	}
+	
+	
+	
 	public static void printWifiNetworks() {
 		for (int i = 7; i < spliter.length; i += 4) 
 		{
